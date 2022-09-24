01ff8320cca9b09ccac126d55f1b4bf624f5e9cd
MarioRP-01 <marlorosadron@gmail.com>
2022-09-24 14:28:48 +0200

    Activity 2 of Subject 2 done.

--- subject_2/activity_2.sh
+++ subject_2/activity_2.sh
@@ -2,26 +2,24 @@
 
 SCRIPT_REGEX=*.sh
 
-
-if test $# -lt 1
+if test $# -gt 1
 then
-	DIRECTORY=$PWD
-elif
-
-	if test $# -ne 1
-	then
-		echo "Wrong number of parameters. None or one directory must be passed as parameter";
-		exit 1;
-	fi
+	echo "Wrong number of parameters" ; exit 1;
 fi
 
-if test -d $1
+if test $# -eq 0
+then 
+	DIR=$PWD
+elif test -d $1
 then
-	echo "One directory must be passed as parameter" ; exit 1;
+	DIR=$1
+else
+	echo "A directory must be received" ; exit 1
 fi
 
-DIRECTORY=$1
+for ELEMENT in $(find $DIR -name "*.sh")
+do
+	chmod u+x $ELEMENT
+done
 
-for $ELEMENT in $(find $DIRECTORY)
-
-	
+exit 0;
