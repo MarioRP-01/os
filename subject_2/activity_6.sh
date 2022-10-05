5d9e1cac56b19979816e6311e314e56930a99d1b
MarioRP-01 <marlorosadron@gmail.com>
2022-10-05 10:07:28 +0200

    Activity_6 almost done

--- subject_2/activity_6.sh
+++ subject_2/activity_6.sh
@@ -5,7 +5,7 @@ if test $# -ne 1 ; then
 fi
 
 if ! test -f $1 ; then
-  echo "Given parameter must be a file"; exi
+  echo "Given parameter must be a file"; exit 2
 fi
 
 clear_temp() {
@@ -14,6 +14,48 @@ clear_temp() {
   fi
 }
 
+extract() {
+  if test -d $2 ; then
+    tar -xf $1 -C $2
+  fi
+}
+
+remove_bigger() {
+  if ! test -d $1 ; then
+    return
+  fi
+  
+  for file in $(ls $1) ; do
+    file_size=$(wc -c $file | cut -d " " -f 1)        
+
+    if test $file_size -gt 8000 ; then
+      rm $temp"/"$file
+      echo Archivo borrado: $file
+    else
+      echo Archivo guardado: $file
+    fi
+  done
+} 
+
+compress() {
+  if ! test -d $1 ; then
+    return
+  fi
+  echo $1"/*"
+  tar -cf comp.tar $1"/*"
+}
+
+file_comp=$1
 temp=$(mktemp -dt activity_6_XXX)
+
+# I don't understand why this doesn't get my parameters
+extract $file_comp $temp
+ls $temp
+
+remove_bigger $temp
+ls $temp
+
+compress $temp
+ls
 
 clear_temp $temp
