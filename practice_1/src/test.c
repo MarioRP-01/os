d2d1b94a70aa94f6f8b9e39b6108481beec59541
MarioRP-01 <marlorosadron@gmail.com>
2022-11-04 14:46:24 +0100

    Implement functionality test.c, not tested.

--- practice_1/src/test.c
+++ practice_1/src/test.c
@@ -1,16 +1,53 @@
-#include "libreria.h"
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "../lib/libreria.h"
+
+int isNatural(char * number);
 
 int main(int argc, char *argv[])
 {
   int number_of_parameters = argc - 1;
+  char * function_name;
+  int N = 10;
 
-  // TODO: Parameter chek
-  if (number_of_parameters < 1) {
+  if (number_of_parameters < 1 || number_of_parameters > 2) {
     printf("Wrong number of parameters\n");
     return 1;
   }
 
-  // TODO: Redirect to librarie's function
-  printf("It works\n");
+  if (number_of_parameters == 2  && !isNatural(argv[number_of_parameters])) {
+    printf("The second paramter must be a Natural number");
+    return 1;
+  }  
+
+  if (number_of_parameters == 2) {
+    N = atoi(argv[number_of_parameters]);
+  }
+
+  function_name = argv[1];
+
+  if (strcmp(function_name, "head")) head(N); 
+  else if (strcmp(function_name, "tail")) tail(N);
+  else if (strcmp(function_name, "longlines")) longlines(N);
+  else {
+    printf("The first parameter must be a valid funcion (head, tail, longlines).");
+    return 1;
+  }
   return 0;
 }
+
+int isNatural(char * number) {
+  int isNatural = 1;
+
+  int i = 0;
+  while (isNatural && number[i] == '\0') {
+    isNatural = isdigit(number[i]);
+    i++;
+  }
+
+  return isNatural;
+}
+
