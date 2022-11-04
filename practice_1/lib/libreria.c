362079f92603d0769eb082161c15b2488590acfc
MarioRP-01 <marlorosadron@gmail.com>
2022-11-04 13:40:37 +0100

    Move libreria.h and libreria.c

---
+++ practice_1/lib/libreria.c
@@ -0,0 +1,14 @@
+int head(int N){
+  int result = 0;
+  return result;
+}
+
+int tail(int N){
+  int result = 0;
+  return result;
+}
+
+int longlines(int N){
+  int result = 0;
+  return result;
+}
