a6ef650eb9e91b85eda0efbdc03fec5979d8e442
MarioRP-01 <marlorosadron@gmail.com>
2022-12-03 13:32:58 +0100

    Add Redirections struct. Create execute_command

--- practice_2/myshell/myshell.c
+++ practice_2/myshell/myshell.c
@@ -1,5 +1,8 @@
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #include <unistd.h>
 
 #include "parser.h"
@@ -13,6 +16,7 @@
 #define ERROR_BAD_ARGUMENTS "argumento: No se admiten argumentos"
 #define ERROR_INVALID_FUNCTION "mandato: No se encuentra el mandato"
 #define ERROR_FILE "fichero: Error. Descripción del error"
+#define ERROR_FORK "fork: Error. No se pudo crear el hijo"
 
 // -----
 // BOOL
@@ -42,11 +46,32 @@ Array array(void *value, size_t size) {
 }
 
 // -----
+// REDIRECTIONS
+// -----
+typedef struct Redirections {
+  FILE *stdin;
+  FILE *stdout;
+  FILE *stderr;
+} Redirections;
+
+Redirections redirections(FILE *stdin, FILE *stdout, FILE *stderr) {
+  Redirections redirections;
+  redirections.stdin = stdin;
+  redirections.stdout = stdout;
+  redirections.stderr = stderr;
+  return redirections;
+}
+
+// -----
 // FUNCTION DECLARATION
 // -----
 
 // promt
 Bool prompt(Array *buffer, char *cwd);
+
+// execute commands
+void execute_line(tline);
+void execute_command(tcommand command, Redirections redirections);
 
 // extension parser
 Bool isValidLine(tline *line);
@@ -60,7 +85,7 @@ int main(int argc, char **argv) {
 
   if (argc != 1) {
     fprintf(stderr, "%s\n", ERROR_BAD_ARGUMENTS);
-    exit(1);
+    exit(-1);
   }
 
   char *cwd = getcwd(NULL, (size_t)0);
@@ -73,7 +98,6 @@ int main(int argc, char **argv) {
       fprintf(stderr, "%s\n", ERROR_INVALID_FUNCTION);
       continue;
     }
-    printf("%s\n", line->commands->filename);
   }
 
   free(cwd);
@@ -89,6 +113,21 @@ int main(int argc, char **argv) {
 Bool prompt(Array *buffer, char *cwd) {
   printf("%s %s > ", PROMPT_SHELL, cwd);
   return getline((char **)&buffer->value, &buffer->size, stdin) != -1;
+}
+
+void execute_line(tline line) {}
+
+void execute_command(tcommand command, Redirections redirections) {
+  pid_t pid = fork();
+  int status;
+  if (pid < 0) {
+    fprintf(stderr, "%s\n", ERROR_FORK);
+    exit(-2);
+  } else if (pid == 0) {
+    execv(command.filename, command.argv);
+  } else {
+    wait(&status);
+  }
 }
 
 Bool isValidLine(tline *line) {
